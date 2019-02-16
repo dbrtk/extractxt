e81f74157455fb3d7dac481fefce7b1f441b6dd0
Dominik Bartkowski <dominik.bartkowski@gmail.com>
2019-02-16 19:59:42 +0100

    -

--- extractxt/web/djapp/views.py
+++ extractxt/web/djapp/views.py
@@ -1,6 +1,7 @@
 import json
 import os
 import shlex
+import stat
 import subprocess
 import tempfile
 import uuid
@@ -12,7 +13,7 @@ import requests
 from ...config import (
     ALLOWED_CONTENT_TYPES, CORPUS_ENDPOINT, CORPUS_DATA_ENDPOINT,
     CORPUS_STATUS, CREATE_CORPUS_ENDPOINT, DEFAULT_ENCODING,
-    EXPECTED_FILES_ENDPOINT)
+    EXPECTED_FILES_ENDPOINT, TMP_PATH)
 from ...task import process_files
 
 
@@ -32,10 +33,12 @@ def upload_files(request):
             'unique_id': uuid.uuid4().hex,
             'charset': _file.charset or DEFAULT_ENCODING
         }
-        with tempfile.NamedTemporaryFile(delete=False) as outf:
-            file_data['tmp_file'] = outf.name
+        outf_path = '{}/{}'.format(TMP_PATH, uuid.uuid4().hex)
+        with open(outf_path, 'b+a') as outf:
             for line in _file.readlines():
                 outf.write(line)
+        file_data['tmp_file'] = outf_path
+        os.chmod(outf_path, stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)
 
         ctype = get_mimetype(file_data['tmp_file'])
 
