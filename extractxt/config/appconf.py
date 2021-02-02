c46159f9ad8ec1b6c5727b02c0927408cbe73b82
Dominik Bartkowski <dominikbartkowski@gmail.com>
2021-02-02 22:00:03 +0000

    -

--- extractxt/config/appconf.py
+++ extractxt/config/appconf.py
@@ -46,3 +46,18 @@ CONTENT_TYPES = {
 
 # celery, redis (auth access) configuration
 REDIS_PASS = os.environ.get('REDIS_PASS')
+
+
+# RabbitMQ configuration
+# RabbitMQ rpc queue name
+# These values are defined on the level of docker-compose.
+RPC_QUEUE_NAME = os.environ.get('RPC_QUEUE_NAME', 'extractxt')
+
+# login credentials for RabbitMQ.
+RPC_PASS = os.environ.get('RABBITMQ_DEFAULT_PASS')
+RPC_USER = os.environ.get('RABBITMQ_DEFAULT_USER')
+RPC_VHOST = os.environ.get('RABBITMQ_DEFAULT_VHOST')
+
+# the host to which the rpc broker (rabbitmq) is deployed
+RPC_HOST = os.environ.get('RABBITMQ_HOST')
+RPC_PORT = os.environ.get('RABBITMQ_PORT', 5672)
--- extractxt/config/celeryconf.py
+++ extractxt/config/celeryconf.py
@@ -1,23 +1,25 @@
 
-from .appconf import BROKER_HOST_NAME, REDIS_PASS
+from .appconf import RPC_HOST, RPC_PASS, RPC_PORT, RPC_USER, RPC_VHOST
+
+# broker_url = 'amqp://myuser:mypassword@localhost:5672/myvhost'
+_url = f'amqp://{RPC_USER}:{RPC_PASS}@{RPC_HOST}:{RPC_PORT}/{RPC_VHOST}'
+
+broker_url = _url
+result_backend = 'rpc://'
+
+result_persistent = True
 
 
-_url = f'redis://:{REDIS_PASS}@{BROKER_HOST_NAME}:6379/0'
+result_expires = 30
+timezone = 'UTC'
 
-BROKER_URL = _url
-CELERY_RESULT_BACKEND = _url
+imports = ('extractxt.tasks', )
 
+accept_content = ['json', 'msgpack', 'yaml']
+task_serializer = 'json'
+result_serializer = 'json'
 
-CELERY_TASK_RESULT_EXPIRES = 30
-CELERY_TIMEZONE = 'UTC'
-
-CELERY_IMPORTS = ('extractxt.tasks', )
-
-CELERY_ACCEPT_CONTENT = ['json', 'msgpack', 'yaml']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
-
-CELERY_ROUTES = {
+task_routes = {
 
     'extractxt.tasks.*': {'queue': 'extractxt'},
 
